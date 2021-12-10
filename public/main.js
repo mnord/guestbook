1f5a3cf6af17b2b18b8c9ed32bd3629d92b07620
Marianne Nordlund <marianne.nordlund.syned21jon@tucsweden.se>
2021-12-10 03:17:09 +0100

    Har sparat input till json i textfil och kan skriva ut från textfil till consol

--- public/main.js
+++ public/main.js
@@ -1,9 +1,24 @@
-const form = document.getElementById("reg-form");
-form.addEventListener('submit', registerUser);
+//const form = document.getElementById("reg-form");
+const messages = document.getElementById("msg");
 
+getData();
+
+async function getData() {
+   
+    const response = await fetch('/test');
+    const data = await response.json();
+
+    console.log(data);
+
+}
+
+//form.addEventListener('submit', registerUser);
+
+/*
 function registerUser(e) {
     e.preventDefault();
     const username = document.getElementById("username").value;
     const password = document.getElementById("password").value;
 
 }
+*/
--- server.js
+++ server.js
@@ -1,10 +1,37 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+
+
+app.get('/test', (req, res) => {
+    
+    fs.readFile('test.txt', 'utf-8', function(err, data) {
+        res.json(data);
+    })
+    
+})
+
+
+app.post('/test', (req, res) => {
+    var user = { username: req.body.username, password: req.body.password};
+    var text = JSON.stringify(user);
+    fs.appendFile("test.txt", text, (err) => {
+        if(err) throw err;
+    })
+    res.redirect('/');
+    //res.send("hello");
+
+
+})
+
+
 const PORT = process.env.PORT || 3000;
 
-app.listen(PORT, () => console.log(`Server started localhost:${PORT}`));
+app.listen(PORT, () => console.log(`Server started  at localhost:${PORT}`));
