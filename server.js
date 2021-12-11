c48128db38f1bfde715a3acae1e7a84137592402
Marianne Nordlund <marianne.nordlund.syned21jon@tucsweden.se>
2021-12-11 03:22:09 +0100

    Ändrat i html filen, tagit bort fetch function i main,js, lagt til stil

--- server.js
+++ server.js
@@ -8,28 +8,45 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+let userInput = new Array;
 
 
-app.get('/test', (req, res) => {
+app.post('/', (req, res) => {
     
-    fs.readFile('test.txt', 'utf-8', function(err, data) {
-        res.json(data);
-    })
+    const timestamp = Date.now();
+    const nyinput = {
+        timestamp: timestamp,
+        namn: req.body.username,
+        email: req.body.email,
+        message: req.body.message
+    };
+   
+    const savedData = fs.readFileSync("test.txt");
+
     
-})
+    userInput.push(nyinput);
 
-
-app.post('/test', (req, res) => {
-    var user = { username: req.body.username, password: req.body.password};
-    var text = JSON.stringify(user);
-    fs.appendFile("test.txt", text, (err) => {
+    fs.writeFile("test.txt", JSON.stringify(userInput, null, 2), (err) => {
         if(err) throw err;
-    })
-    res.redirect('/');
+    });
+    userInput = JSON.parse(savedData);
+    
+    console.log(fs.readFileSync("test.txt", "utf8"));
+    
+    
+
+    fs.readFile("./public/index.html", (err, data) => {
+        let nyHTML = data.toString().replace("test", "hejsan");
+
+        res.send(nyHTML);
+        
+    });
+    //res.json(userinput);
+    //res.redirect('/');
     //res.send("hello");
 
 
-})
+});
 
 
 const PORT = process.env.PORT || 3000;
