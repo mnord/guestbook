2ff601cbe67b1893504c5a5cceb2c0dbef0e6a56
Marianne Nordlund <marianne.nordlund.syned21jon@tucsweden.se>
2021-12-12 21:54:11 +0100

    La till felhantering

--- public/main.js
+++ public/main.js
@@ -1,2 +1,66 @@
-//const form = document.getElementById("reg-form");
-const messages = document.getElementById("msg");
+document.getElementById("forum-form").addEventListener("submit", submitForm);
+//Eventuell felhantering om jag hinner
+
+//Kollar att input inte är tomt
+const isRequired = value => value === '' ? false : true;
+
+//Kollar format på email
+const isEmailValid = (email) => {
+    const re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(email);
+}
+
+//Felmeddelande
+function errorMessage (msg) {
+    const displayError = document.getElementById("errormsg");
+    displayError.innerHTML = msg;
+}
+
+function escapeHtml(unsafe)
+{
+    return unsafe
+         .replace(/&/g, "&amp;")
+         .replace(/</g, "&lt;")
+         .replace(/>/g, "&gt;")
+         .replace(/"/g, "&quot;")
+         .replace(/'/g, "&#039;");
+ }
+
+//Kollar så att namn är skrivet, skickar felmeddelande om tomt
+const checkName = () => {
+    const username = document.getElementById("username").value;
+    if (!isRequired(username)) {
+        errorMessage("Namn får inte vara tomt.");
+        return false
+    } else {
+        return true
+    }
+    
+}
+//Kollar email format och skickar felmeddelande
+const checkEmail = () => {
+    const email = document.getElementById("email").value;
+    if (!isEmailValid(email)) {
+        errorMessage("Fel format på email.");
+        return false
+    } else {
+        return true
+    }
+}
+//Kollar att det finns något meddelande skrivet, felmeddelande
+const checkMsg = () => {
+    const msg = document.getElementById("message").value;
+    if (!isRequired(msg)) {
+        errorMessage("Skriv ett meddelande.");
+        return false
+    } else {
+        return true
+    }
+}
+
+//Skickar inte in om någon av check-functionerna ger false
+ function submitForm(e) {
+    if (!checkName() || !checkEmail() || !checkMsg()) {
+        e.preventDefault();
+    }
+ }
--- server.js
+++ server.js
@@ -8,43 +8,56 @@ app.use(express.urlencoded({extended: true}));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-let userInput = new Array;
-
 
 app.post('/', (req, res) => {
-    
+    //Tid när inlägg tas emot
     const timestamp = Date.now();
+//Läser in fil-innehåll till en variabel
+    const testar = fs.readFileSync('test.txt');
+//Lägger till en array med alla inputs
+    let userInput = JSON.parse(testar);
+
+    //Kollar skadlig input
+    function escapeHtml(unsafe)
+    {
+    return unsafe
+         .replace(/&/g, "&amp;")
+         .replace(/</g, "&lt;")
+         .replace(/>/g, "&gt;")
+         .replace(/"/g, "&quot;")
+         .replace(/'/g, "&#039;");
+    }
+//Formaterar input från formulär
     const nyinput = {
         timestamp: timestamp,
-        namn: req.body.username,
-        email: req.body.email,
-        message: req.body.message
+        namn: escapeHtml(req.body.username),
+        email: escapeHtml(req.body.email),
+        message: escapeHtml(req.body.message)
     };
-   
-    const savedData = fs.readFileSync("test.txt");
-
-    
+  //Lägger till ny input till array
     userInput.push(nyinput);
 
+//Skriver inputs till filen test.txt i JSON format
     fs.writeFile("test.txt", JSON.stringify(userInput, null, 2), (err) => {
         if(err) throw err;
-    });
-    userInput = JSON.parse(savedData);
-    
-    console.log(fs.readFileSync("test.txt", "utf8"));
-    
-    
-
+//Läser från filen med sparade inlägg och formaterar
+    const userPosts = JSON.parse(fs.readFileSync("test.txt", "utf-8"));
+    let displayPosts = "";
+    //console.log(userPosts);
+    for (let i = 0; i < userPosts.length; i++) {
+        displayPosts += `<div><h3>${new Date(userPosts[i].timestamp).toLocaleDateString()} ${new Date(userPosts[i].timestamp).toLocaleTimeString()}</h3><h4>FROM: ${userPosts[i].namn} EMAIL: ${userPosts[i].email}</h4><p>${userPosts[i].message}</p></div>`;
+        
+    }
+//Ersätter data i html filen med inlägg som sparats i test.txt och skickar
+//den nya ändrade html file.
     fs.readFile("./public/index.html", (err, data) => {
-        let nyHTML = data.toString().replace("test", "hejsan");
+        let nyHTML = data.toString().replace("test", displayPosts);
 
         res.send(nyHTML);
         
     });
-    //res.json(userinput);
-    //res.redirect('/');
-    //res.send("hello");
 
+    });
 
 });
 
